a4fb1c22d31d4062d7d40ad0ffdb9ff58c66341c
Thibaut C <t.chevallier11@gmail.com>
2020-01-21 09:03:41 +0100

    Création de la page login

--- app/src/main/java/fr/isen/chevallier/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/chevallier/androidtoolbox/HomeActivity.kt
@@ -3,7 +3,7 @@ package fr.isen.chevallier.androidtoolbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
