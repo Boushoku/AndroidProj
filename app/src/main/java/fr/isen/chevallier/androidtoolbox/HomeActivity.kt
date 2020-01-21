054ca070b20629b8b46f279d37696e208cb06e50
Thibaut C <t.chevallier11@gmail.com>
2020-01-21 11:00:11 +0100

    redirection vers lifecycle

--- app/src/main/java/fr/isen/chevallier/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/chevallier/androidtoolbox/HomeActivity.kt
@@ -1,12 +1,20 @@
 package fr.isen.chevallier.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class HomeActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val buttonLifeCycle = findViewById<ImageButton>(R.id.buttonLifeCycle)
+        val intentLifeCycle = Intent(this, LifeCycleActivity::class.java)
+        buttonLifeCycle.setOnClickListener{
+            startActivity(intentLifeCycle)
+        }
     }
 }
