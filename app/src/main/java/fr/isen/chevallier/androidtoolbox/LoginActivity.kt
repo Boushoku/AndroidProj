627cba83cbe6e803dc6f1dd0286b7f6119696473
Thibaut C <t.chevallier11@gmail.com>
2020-01-21 09:50:25 +0100

    test des valeurs des edits text et affiche un message en fonction du résultat

--- app/src/main/java/fr/isen/chevallier/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/chevallier/androidtoolbox/LoginActivity.kt
@@ -2,11 +2,30 @@ package fr.isen.chevallier.androidtoolbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var goodId = "admin"
+        var goodPw = "123"
+        var buttonValidate = findViewById<Button>(R.id.validateButton)
+        var identifiant = findViewById<EditText>(R.id.identifiant)
+        var password = findViewById<EditText>(R.id.password)
+        buttonValidate.setOnClickListener{
+            if (identifiant.text.toString() == goodId && password.text.toString() == goodPw){
+                Toast.makeText(this, "Welcome " + identifiant.text.toString(), Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(this, "Wrong username/password", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
+
 }
