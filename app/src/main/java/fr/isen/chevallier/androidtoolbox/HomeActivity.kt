cc2a364ef74b569ea4cf4e1e918cc23980596bda
Thibaut C <t.chevallier11@gmail.com>
2020-01-27 11:28:34 +0100

    Shared Preferences

--- app/src/main/java/fr/isen/chevallier/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/chevallier/androidtoolbox/HomeActivity.kt
@@ -1,20 +1,33 @@
 package fr.isen.chevallier.androidtoolbox
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 
 class HomeActivity : AppCompatActivity(){
+    private val sharedPreferencesFile = "kotlinsharedpreference"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPreferencesFile,
+            Context.MODE_PRIVATE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val buttonLifeCycle = findViewById<ImageButton>(R.id.buttonLifeCycle)
         val intentLifeCycle = Intent(this, LifeCycleActivity::class.java)
         buttonLifeCycle.setOnClickListener{
             startActivity(intentLifeCycle)
+        }
+        val buttonDeconnexion = findViewById<Button>(R.id.decoButton)
+        buttonDeconnexion.setOnClickListener(){
+            val editor = sharedPreferences.edit()
+            editor.clear()
+            editor.apply()
+            val intentLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentLogin)
         }
     }
 }
--- app/src/main/java/fr/isen/chevallier/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/chevallier/androidtoolbox/LoginActivity.kt
@@ -1,6 +1,8 @@
 package fr.isen.chevallier.androidtoolbox
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,10 +11,14 @@ import android.widget.TextView
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    private val sharedPreferencesFile = "kotlinsharedpreference"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPreferencesFile,
+            Context.MODE_PRIVATE)
         val goodId = "admin"
         val goodPw = "123"
         val buttonValidate = findViewById<Button>(R.id.validateButton)
@@ -21,6 +27,20 @@ class LoginActivity : AppCompatActivity() {
         val intentHomePage = Intent(this, HomeActivity::class.java)
         buttonValidate.setOnClickListener{
             if (identifiant.text.toString() == goodId && password.text.toString() == goodPw){
+
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+                editor.putString("id_key",goodId)
+                editor.putString("password_key",goodPw)
+                editor.apply()
+                editor.commit()
+
+                val sharedIdValue = sharedPreferences.getString("id_key","defaultid")
+                val sharedNameValue = sharedPreferences.getString("name_key","defaultname")
+                Toast.makeText(this, "Saved:Email" + sharedIdValue.toString() +"\nSaved:Password" + sharedNameValue.toString(), Toast.LENGTH_LONG).show()
+
+
+
                 startActivity(intentHomePage)
                 Toast.makeText(this, "Welcome " + identifiant.text.toString(), Toast.LENGTH_LONG).show()
             }
